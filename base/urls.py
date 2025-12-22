d8008cd2871e64764c268a7dd5bf9afddc6b91ae
Atharva <athnivatkar908@gmail.com>
2025-12-22 21:49:51 +0530

    Day 2

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('',views.home, name = "home"),
-    path('room/',views.room, name = 'room'),
+    path('room/<str:pk>/',views.room, name = 'room'),
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Room
+
+rooms = [
+    {'id': 1, 'name': 'Lets study python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Home Page")
-def room(request):
-    return HttpResponse("Room Page")
+    rooms = Room.objects.all()
+    context = {'rooms' : rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {'room' : room}
+
+    return render(request, 'base/room.html',context)
