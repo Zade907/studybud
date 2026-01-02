47b182d37fa68b11024b90409f761c0413a33205
Atharva <athnivatkar908@gmail.com>
2026-01-03 02:20:04 +0530

    Day 6 continued - Associating Rooms with Hosts and Adding CSS

--- base/forms.py
+++ base/forms.py
@@ -5,3 +5,4 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'  #include all fields from Room model
+        exclude = ['host', 'participants']  #exclude host and participants fields
